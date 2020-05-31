8bebc9ac093f09ea48ed77da65f6242d2a0c4d2c
Izzet <izzetertas@gmail.com>
2020-05-31 16:39:08 +0100

    validateEmailAddress implemantation

---
+++ web-app/src/utils/validateEmailAddress.js
@@ -0,0 +1,7 @@
+const validateEmailAddress = (term) => {
+    let regexPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regexPattern.test(term)
+}
+
+export default validateEmailAddress
+
