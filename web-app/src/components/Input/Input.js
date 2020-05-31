129e42992409bc9740b3ecd15240743569a559f5
Izzet <izzetertas@gmail.com>
2020-05-31 16:19:55 +0100

    Input component implementation

---
+++ web-app/src/components/Input/Input.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Input = styled.input`
+  width: 300px;
+  height: 35px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  margin: 4px 0;
+  padding: 0 8px;
+`
+
+export default Input
+
