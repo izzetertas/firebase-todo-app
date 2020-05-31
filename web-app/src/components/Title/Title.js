6561db38cf5490801017b177c1e6a6d3896280a1
Izzet <izzetertas@gmail.com>
2020-05-31 16:19:10 +0100

    Title component implementation

---
+++ web-app/src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-family: 'Raleway', sans-serif;
+  font-weight: 600;
+  color: #4d4d4d;
+  font-size: 2.2em;
+  text-align: center;
+`
+
+export default Title
+
