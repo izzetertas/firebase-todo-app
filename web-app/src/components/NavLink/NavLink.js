996e3708692c2b81074b07c3c4894ae7425e9512
Izzet <izzetertas@gmail.com>
2020-05-31 16:20:30 +0100

    NavLink component implementation

---
+++ web-app/src/components/NavLink/NavLink.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const NavLink = styled.a`
+  height: 100%;
+  color: #5995ef;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  white-space: nowrap;
+  text-decoration: none;
+  font-size: 14px;
+  margin-bottom: 16px;
+`
+
+export default NavLink
+
