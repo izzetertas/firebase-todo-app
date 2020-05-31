bff6813a965f1772d9e44c774481bb575d6a0853
Izzet <izzetertas@gmail.com>
2020-05-31 16:19:29 +0100

    Form component implementation

---
+++ web-app/src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Form = styled.form`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
+  margin: 16px auto;
+  border: 1px solid grey;
+`
+
+export default Form
+
