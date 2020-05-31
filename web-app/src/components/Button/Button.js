d5c44ec5ceec4de63389ada48fe70d665a026e91
Izzet <izzetertas@gmail.com>
2020-05-31 16:19:40 +0100

    Button component implementation

---
+++ web-app/src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+width: 300px;
+height: 40px;
+background-color: #5995ef;
+color: #fff;
+border-radius: 3px;
+margin: 8px 0;
+font-size: 16px
+`
+
+export default Button
+
